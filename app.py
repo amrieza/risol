317c6ddb27d7be2096362017a4834ece13635266
amrieza <113014327+amrieza@users.noreply.github.com>
2024-01-04 01:53:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 import streamlit as st
+from flask import Flask, render_template
 
-st.title("RISOL MAYO")
+app = Flask(__name__)
 
-# Bagian Home
-st.header("Home")
-st.write("Nikmati kelezatan Risol Mayo kami yang lezat dan renyah. Segera pesan sekarang!")
-st.button("GET IT NOW")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Bagian About
-st.header("About Product")
-st.image("images/risol.jpg", caption="Risol Mayo", use_column_width=True)
-st.write("Risol mayonnaise adalah jajanan tradisional berbentuk gulungan yang memiliki berbagai isian, dari ragout, telur, hingga smoked beef.")
-st.write("Biasanya, risol disajikan dengan cara digoreng dan dimakan bersama saus sambal. Perpaduan rasa renyah dan creamy pada risol ini menciptakan rasa yang gurih.")
-
-# Bagian Footer
-st.header("Contact & Follow Us")
-st.image("images/logo.png", caption="Risol Mayo Logo", use_column_width=True)
-st.write("Follow us on Instagram: [RisolMayoOfficial](https://www.instagram.com/RisolMayoOfficial/)")
-st.write("For inquiries, contact us at: info@risolmayo.com")
-
-# Copyright
-st.footer("Copyright © 2022 Amrieza Herdiyanto")
+if __name__ == '__main__':
+    app.run(debug=True
